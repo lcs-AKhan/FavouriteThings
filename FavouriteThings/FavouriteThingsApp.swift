b39e7b57a54cae2514203d3ea7a12d969f939694
Abdul Khan <akhan@lcs.on.ca>
2020-10-16 15:14:25 -0400

    FavouriteThings App: Completed "About me" View

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Abdul Ahad Khan on 2020-10-15.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
